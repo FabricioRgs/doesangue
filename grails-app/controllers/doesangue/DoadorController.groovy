bb194a17722a39b66f7d9e1c07de4eb27e91985b
Fabricio Ubuntu Sisaut <fabricioxrodrigues@gmail.com>
2020-05-28 11:48:28 -0300

    get by id

--- grails-app/controllers/doesangue/DoadorController.groovy
+++ grails-app/controllers/doesangue/DoadorController.groovy
@@ -2,14 +2,19 @@ package doesangue
 
 class DoadorController {
 
+    static scaffold = Doador
+
     def delete() {
       super.delete()
     }
 
     def index() {
-      render Doador.getAll() as grails.converters.JSON
+      if (params.id > 0) {
+        render Doador.get(params.id) as grails.converters.JSON
+      }
+      else {
+        render Doador.getAll() as grails.converters.JSON
+      }
     }
-
-    static scaffold = Doador
 
 }
